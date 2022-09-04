2352c7715ab32bb105a5be051ce5da683e2805b8
Eric Peterson <ericpeterson91@live.ca>
2022-09-03 18:35:35 -0600

    clear and set interval

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-const squares = document.querySelectorAll('.square')
-const mole = document.querySelector('.mole')
-
-const timeLeft = document.querySelector('#time-left')
-const score = document.querySelector('#score')
-let result = 0
+const squares = document.querySelectorAll(".square");
+const mole = document.querySelector(".mole");
+const start = document.querySelector("#start");
+const pause = document.querySelector("#pause");
+const timeLeft = document.querySelector("#time-left");
+const score = document.querySelector("#score");
+let result = 0;
+let countdown = 60
+let timer;
 
 function randomSquare() {
-    squares.forEach(square => {
-        square.classList.remove('mole')
-    })
-    let randomSquare = squares[Math.floor(Math.random() * 9)]
-    randomSquare.classList.add('mole')
+  squares.forEach((square) => {
+    square.classList.remove("mole");
+  });
+  let randomSquare = squares[Math.floor(Math.random() * 9)];
+  randomSquare.classList.add("mole");
+  countdown--
+    timeLeft.innerText = countdown
 }
 
-randomSquare()
+const startGame = () => {
+    timer = setInterval(randomSquare, 750)
+}
+
+const stopGame = () => {
+    clearInterval(timer)
+}
+
+start.addEventListener('click', startGame)
+pause.addEventListener('click', stopGame);
